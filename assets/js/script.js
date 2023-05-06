d03ba6bbe9b06767119eca7ed5fd7e8827f1563a
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-05-06 14:26:41 +0000

    hamburger menu inplace needs sorting out the content.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown-content");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+/* modal button at the bottom of the page!  */ 
+
+
 const openButton = document.querySelector("[data-open-modal]");
 const closeButton = document.querySelector("[data-close-modal]");
 const modal = document.querySelector("[data-modal]");
@@ -9,3 +21,6 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     modal.close();
 });
+
+
+
